bc2f9e3b2b6dc7d5ba5188f3c14a01c99270e20e
NickRaise <105662507+NickRaise@users.noreply.github.com>
2023-08-23 21:45:57 +0530

    Added another approach to solve Search_element_in _rotated_sorted_array

--- Binary_Search/Search_Element_in_Rotated_Sorted_Array.cpp
+++ Binary_Search/Search_Element_in_Rotated_Sorted_Array.cpp
@@ -43,7 +43,7 @@ int binary_search(vector<int>& arr, int left, int right, int k) {
 }
 
 // Function to search for an element in a rotated sorted array
-int search(vector<int>& arr, int n, int k) {
+int search__Approach1(vector<int>& arr, int n, int k) {
     int left = 0, right = n - 1;
     int mid;
 
@@ -77,6 +77,44 @@ int search(vector<int>& arr, int n, int k) {
     return ans;
 }
 
+int search__Approach2(vector<int>& arr, int n, int k)
+{
+    int left = 0, right = n - 1;
+    int mid;
+    while (left <= right) {
+        mid = left + (right - left) / 2;
+
+        // If the middle element is equal to the target 'k', return its index
+        if (arr[mid] == k) {
+            return mid;
+        }
+        else if (arr[mid] >= arr[left]) {
+            // If the left half of the array is sorted
+
+            // Check if the target 'k' lies within the sorted range of the left half
+            if (arr[left] <= k && k <= arr[mid - 1]) {
+                right = mid - 1; // Update 'right' to search in the left half
+            }
+            else {
+                left = mid + 1; // Update 'left' to search in the right half
+            }
+        }
+        else {
+            // If the right half of the array is sorted
+
+            // Check if the target 'k' lies within the sorted range of the right half
+            if (arr[mid] <= k && k <= arr[right]) {
+                left = mid + 1; // Update 'left' to search in the right half
+            }
+            else {
+                right = mid - 1; // Update 'right' to search in the left half
+            }
+        }
+    }
+    return -1; // Return -1 if the target 'k' is not found in the array
+}
+
+
 
 int main() {
     int n, k;
@@ -89,7 +127,7 @@ int main() {
         cin >> arr[i];
     }
     
-    int index = search(arr, n, k);
+    int index = search__Approach1(arr, n, k);
     if (index != -1) {
         cout << "Element " << k << " found at index " << index << endl;
     } else {
